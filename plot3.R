568b05cadc484bc68fb05371412ac0657435eeb9
ElizabethArangoR <66488484+ElizabethArangoR@users.noreply.github.com>
2021-01-08 19:11:19 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,20 +1,37 @@
+# Reading data from a .txt file
+
 HPC <- read.table("household_power_consumption.txt", header = T,
                   sep=";", comment.char="%", stringsAsFactors=FALSE, na.strings="?")
-dim(HPC)
-dataplot<- subset(HPC,HPC$Date=="1/2/2007"|HPC$Date=="2/2/2007")
-datetime  <- strptime(paste(dataplot$Date, dataplot$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+
+head(HPC) # Looking the headers of the dataset.
+
+# Filtering on the 2 days of February.
+
+datafeb <- subset(HPC, HPC$Date=="1/2/2007"|HPC$Date=="2/2/2007")
+
+# Adding a new column called datetime including date and time.
+
+datafeb$datetime  <- strptime(paste(datafeb$Date, datafeb$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+
+# Creating a function with the plot.
 
 plot3 <- function() {
-  plot(datetime,dataplot$Sub_metering_1,type="n",xlab = "",ylab="Energy sub metering")
-  lines(datetime,dataplot$Sub_metering_1,col="black")
-  lines(datetime,dataplot$Sub_metering_2,col="red")
-  lines(datetime,dataplot$Sub_metering_3,col="blue")
+  # Calling the basic plot functions
+  plot(datafeb$datetime, datafeb$Sub_metering_1,type="n",
+       xlab = "",
+       ylab="Energy sub metering")
+  lines(datafeb$datetime, datafeb$Sub_metering_1,col="black")
+  lines(datafeb$datetime, datafeb$Sub_metering_2,col="red")
+  lines(datafeb$datetime, datafeb$Sub_metering_3,col="blue")
+  # Adding legend
   legend("topright",lty=c(1,1,1),
          col=c("black","red","blue"),
          legend=c("Sub_meeting_1",
                   "Sub_meeting_2",
                   "Sub_meeting_3"))
+  # Creating PNG
   dev.copy(png, file="plot3.png", width=480, height=480)
+  # Closing plot
   dev.off()
 }
 
